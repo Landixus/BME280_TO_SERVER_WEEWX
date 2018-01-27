58bd631ec30166291347a1dd94df1e7d7673d6c1
Andreas <Landixus@users.noreply.github.com>
2018-01-27 08:44:32 +0100

    Create bme.php

---
+++ bme.php
@@ -0,0 +1,14 @@
+<?php
+     $client_data = file_get_contents("php://input");
+     $r = explode("&",$client_data);
+     $handle = fopen("data/bme.txt", w);
+     for ($i = 0; $i < count($r); $i++)
+     {
+     echo $r[$i] . "\n";
+     fwrite($handle, $r[$i]);
+     fwrite($handle, ",");
+     };
+     header("Content-type: text/json", TRUE, 200);
+     fclose($handle);
+     echo "Success\n";
+?>
